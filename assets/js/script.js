11952d995e84c7e9085df1b4b0ca12c5314707b4
schultzy192 <darthsidious2011@gmail.com>
2016-05-02 08:19:03 +1200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,8 +68,6 @@ $(document).ready(function(){
                                 var currentForecast = json.days[0].forecast;
                                 $('.currentForecast').text(currentForecast);
                             });
-                            
-						}
 					}
           }
       }
